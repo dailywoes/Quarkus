83c6f2971cf1f19167e2ae12fd5ec18d947f8385
John Tex <johnrteixeira@gmail.com>
2024-10-16 20:12:31 -0400

    Application Scoped example

---
+++ rest-book/src/main/java/org/agoncal/quarkus/starting/BookRepository.java
@@ -0,0 +1,24 @@
+package org.agoncal.quarkus.starting;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class BookRepository {
+
+    public List<Book> getAllBooks() {
+        return List.of(
+                new Book(1, "Understanding Quarkus", "Antonio", 2020, "IT"),
+                new Book(2, "Practising Quarkus", "Antonio", 2020, "IT"),
+                new Book(3, "Effective Java", "Josh Bloch", 2001, "IT"),
+                new Book(4, "Thinking in Java", "Bruce Eckel", 1998, "IT")
+        );
+    }
+
+    public Optional<Book> getBook(int id) {
+        return getAllBooks().stream().filter(book -> book.id == id).findFirst();
+    }
+}
