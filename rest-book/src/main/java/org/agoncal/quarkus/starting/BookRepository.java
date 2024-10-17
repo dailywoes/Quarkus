4f6f9e9da039dda23807ae4b87c2fae12315c204
John Tex <johnrteixeira@gmail.com>
2024-10-16 21:49:40 -0400

    Quarkus Configuration Example

--- rest-book/src/main/java/org/agoncal/quarkus/starting/BookRepository.java
+++ rest-book/src/main/java/org/agoncal/quarkus/starting/BookRepository.java
@@ -1,6 +1,7 @@
 package org.agoncal.quarkus.starting;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 import java.util.List;
@@ -9,12 +10,15 @@ import java.util.Optional;
 @ApplicationScoped
 public class BookRepository {
 
+    @ConfigProperty(name = "books.genre", defaultValue = "Sci-Fi")
+    String genre;
+
     public List<Book> getAllBooks() {
         return List.of(
-                new Book(1, "Understanding Quarkus", "Antonio", 2020, "IT"),
-                new Book(2, "Practising Quarkus", "Antonio", 2020, "IT"),
-                new Book(3, "Effective Java", "Josh Bloch", 2001, "IT"),
-                new Book(4, "Thinking in Java", "Bruce Eckel", 1998, "IT")
+                new Book(1, "Understanding Quarkus", "Antonio", 2020, genre),
+                new Book(2, "Practising Quarkus", "Antonio", 2020, genre),
+                new Book(3, "Effective Java", "Josh Bloch", 2001, genre),
+                new Book(4, "Thinking in Java", "Bruce Eckel", 1998, genre)
         );
     }
 
