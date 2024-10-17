4ce1b6a3b4943d8e6f9b78d038e1b9e25c71c9a6
John Tex <johnrteixeira@gmail.com>
2024-10-16 21:36:39 -0400

    Quarkus Test Example 2

--- rest-book/src/test/java/org/agoncal/quarkus/starting/BookResourceTest.java
+++ rest-book/src/test/java/org/agoncal/quarkus/starting/BookResourceTest.java
@@ -25,11 +25,26 @@ class BookResourceTest {
     @Test
     public void shouldCountAllBooks() {
         given()
-                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN)
                 .when()
                 .get("/api/books/count")
                 .then()
                 .statusCode(200)
                 .body(is("4"));
+    }
+
+    @Test
+    public void shouldGetABook() {
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .pathParam("id", 1)
+                .when()
+                .get("/api/books/{id}")
+                .then()
+                .statusCode(200)
+                .body("title", is("Understanding Quarkus"))
+                .body("author", is("Antonio"))
+                .body("yearOfPublication", is(2020))
+                .body("genre", is("IT"));
     }
 }
